73bbe9454d430277c73844274e0032b11ad3b864
Felipe Maion <felipe.maion@gmail.com>
2024-02-11 13:02:14 -0300

    Initial commit

--- hackathon/manager/models.py
+++ hackathon/manager/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here
 
+class Manager(models.model):
+    user_name = models.CharField(max_length=100,default='manager')
+    password = models.CharField(max_length=100)
+    email = models.EmailField(default='manager_email@email.com')
